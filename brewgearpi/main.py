007b413b7b267be24672690ab12a8f23a22e52a0
Arjan Molenaar <gaphor@gmail.com>
2013-05-13 16:34:26 +0200

    Add handler for all log data.

--- brewgearpi/main.py
+++ brewgearpi/main.py
@@ -6,17 +6,20 @@ import webui
 
 from tornado import ioloop
 
-INTERVAL = 5000     # ms
+INTERVAL = 500     # ms
 
 def main(io):
     mainloop = ioloop.IOLoop.instance()
 
-    log = logger.Logger(io, logger.json_appender(sys.stdout))
-    ioloop.PeriodicCallback(log, INTERVAL, mainloop).start()
+    log_file = open('session.log', 'a')
+    try:
+        log = logger.Logger(io, logger.json_appender(log_file))
+        ioloop.PeriodicCallback(log, INTERVAL, mainloop).start()
 
-    webui.setup(log, mainloop)
+        webui.setup(log, mainloop)
 
-    mainloop.start()
-
+        mainloop.start()
+    finally:
+        log_file.close()
 
 # vim:sw=4:et:ai
--- brewgearpi/webui.py
+++ brewgearpi/webui.py
@@ -16,9 +16,19 @@ def setup(logger, mainloop):
                 self.set_status(204) # No data
                 self.flush()
 
+    class AllReadingsHandler(tornado.web.RequestHandler):
+        def get(self):
+            self.set_header('Content-Type', 'application/json')
+            with open('session.log') as log_file:
+                log_lines = log_file.readlines()
+            self.write('[')
+            self.write(','.join(log_lines))
+            self.write(']')
+            self.flush()
 
     application = tornado.web.Application([
         (r'/reading', ReadingHandler),
+        (r'/readings/all', AllReadingsHandler),
         (r'/(..*)', tornado.web.StaticFileHandler, {'path': 'static'}),
         (r'/$', tornado.web.RedirectHandler, {"url": "/index.html"})
         ])
