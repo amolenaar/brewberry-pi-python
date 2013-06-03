b17b0fd0b73e6d0cf085fd9219d81ebc80d124d9
Arjan Molenaar <gaphor@gmail.com>
2013-06-03 18:26:39 +0200

    Added AngularJs and a simple controller.

---
+++ static/js/controls.js
@@ -0,0 +1,12 @@
+
+function Controls ($scope) {
+
+    $scope.startHeater = function () {
+        console.log('start heating');
+    };
+    $scope.stopHeater = function () {
+        console.log('stop heating');
+    };
+}
+
+// vim:sw=4:et:ai
