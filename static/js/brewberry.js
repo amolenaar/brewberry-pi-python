5b53aa36a3398c05d8a4c7e2ccd22be63c4fb474
Arjan Molenaar <gaphor@gmail.com>
2013-06-06 07:24:25 +0200

    Moved controllers in one file.

--- static/js/brewberry.js
+++ static/js/brewberry.js
@@ -2,16 +2,24 @@
 
 angular.module('brewberry', ['brewberry.directive', 'brewberry.service'])
     .controller('Logger', function ($scope, feed) {
-
-
         var id = feed(function (sample) {
             if (sample) {
-                console.log('sample', sample, $scope);
                 $scope.sample = sample;
                 $scope.$apply();
             }
         });
 
+    })
+
+    .controller('Controls', function ($scope) {
+
+        $scope.startHeater = function () {
+            console.log('start heating');
+        };
+        $scope.stopHeater = function () {
+            console.log('stop heating');
+        };
     });
+
 
 // vim:sw=4:et:ai
