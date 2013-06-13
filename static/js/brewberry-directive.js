b51c0b723f8bbd8d49171a6de2a4f3319385bd0d
Arjan Molenaar <gaphor@gmail.com>
2013-06-13 21:15:50 +0200

    Modularized code even more.

--- static/js/brewberry-directive.js
+++ static/js/brewberry-directive.js
@@ -3,97 +3,100 @@
 angular.module('brewberry.directive', [])
     .directive('logChart', function() {
         return {
-            restrict: 'E',
-            template: '<div class="log-chart"></div>',
-            transclude:true,
-            replace: true,
+            scope: true,
 
-            link: function (scope, element, attrs) {
-                var HISTORY_DEPTH = 2 * 3600 * 1000; // 2 hours of history
+            link: { 
+                pre: function (scope, element, attrs) {
+                    var chart = new Highcharts.Chart({
+                        chart: {
+                            renderTo: element[0],
+                            zoomType: 'xy',
+                            type: 'spline',
+                            backgroundColor: 'transparent',
+                            animation: Highcharts.svg // don't animate in old IE
+                        },
 
-                var chart = new Highcharts.Chart({
-                    chart: {
-                        renderTo: element[0],
-                        zoomType: 'xy',
-                        type: 'spline',
-                        backgroundColor: 'transparent',
-                        animation: Highcharts.svg // don't animate in old IE
-                    },
-
-                    title: {
-                        text: ''
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    xAxis: [{
-                        type: 'datetime',
                         title: {
-                            text: 'Time'
+                            text: ''
                         },
-                    }],
-                    yAxis: [{
-                        labels: {
-                            style: {
-                                color: '#89A54E'
-                            }
+                        credits: {
+                            enabled: false
                         },
-                        title: {
-                            text: 'Temperature',
-                            style: {
-                                color: '#89A54E'
-                            }
+                        legend: {
+                            enabled: false
                         },
-                        opposite: true
-                    }, {
-                        gridLineWidth: 0,
-                        title: {
-                            text: '',
-                            style: {
-                                color: '#A74572'
+                        xAxis: [{
+                            type: 'datetime',
+                            title: {
+                                text: 'Time'
                             },
-                        },
-                        labels: {
-                            formatter: function() {
-                                return this.value === 0 ? 'Off' : (this.value === 1 ? 'On' : '');
+                        }],
+                        yAxis: [{
+                            labels: {
+                                style: {
+                                    color: '#89A54E'
+                                }
                             },
-                            style: {
-                                color: '#A74572'
+                            title: {
+                                text: 'Temperature',
+                                style: {
+                                    color: '#89A54E'
+                                }
                             },
+                            opposite: true
+                        }, {
+                            gridLineWidth: 0,
+                            title: {
+                                text: '',
+                                style: {
+                                    color: '#A74572'
+                                },
+                            },
+                            labels: {
+                                formatter: function() {
+                                    return this.value === 0 ? 'Off' : (this.value === 1 ? 'On' : '');
+                                },
+                                style: {
+                                    color: '#A74572'
+                                },
+                            },
+                            min: 0,
+                            max: 1,
+                            //tickInterval: 1,
+                            //opposite: true
+                        }],
+                        tooltip: {
+                            shared: true
                         },
-                        min: 0,
-                        max: 1,
-                        //tickInterval: 1,
-                        //opposite: true
-                    }],
-                    tooltip: {
-                        shared: true
-                    },
-                    plotOptions: {
-                        series: {
-                            stacking: null,
-                            marker: {
-                                enabled: false
+                        plotOptions: {
+                            series: {
+                                stacking: null,
+                                marker: {
+                                    enabled: false
+                                }
                             }
                         }
-                    }
-                });
+                    });
 
-                var temperature_series = chart.addSeries({
-                    name: 'Temperature',
-                    color: '#89A54E',
+                    scope.chart = chart;
+                }
+            }
+        }
+    })
+    .directive('series', function() {
+        return {
+            restrict: 'E',
+            transclude:true,
+            link: function (scope, element, attrs) {
+                var HISTORY_DEPTH = 2 * 3600 * 1000; // 2 hours of history
+                var hostory_depth = attrs.history || HISTORY_DEPTH
+
+                var series = scope.chart.addSeries({
+                    name: attrs.name,
+                    color: attrs.color,
                     tooltip: {
-                        valueSuffix: ' °C'
+                        valueSuffix: attrs.valuesuffix
                     },
-                    data: []
-                });
-                var heater_series = chart.addSeries({
-                    name: 'Heater',
-                    color: '#4572A7',
-                    yAxis: 1,
                     data: []
                 });
 
@@ -102,28 +105,24 @@ angular.module('brewberry.directive', [])
                     while (series.data[0].x < ts - HISTORY_DEPTH) {
                         series.data[0].remove(false);
                     }
-                    chart.redraw();
+                    //scope.chart.redraw();
                 }
 
-                scope.$watch("sample", function(sample) {
+                var x = attrs.x;
+                var y = attrs.y;
+                scope.$watch(attrs.sample, function(sample) {
                     if (sample) {
-                        var time = Date.parse(sample.time);
-                        temperature_series.addPoint([time, sample.temperature], true, false);
-                        heater_series.addPoint([time, sample.heater ? 1 : 0], true, false);
-                        dropOldData(temperature_series, time);
-                        dropOldData(heater_series, time);
+                        var time = Date.parse(sample[x]);
+                        dropOldData(series, time);
+                        series.addPoint([time, sample[y]], true, false);
                     }
                 });
+                console.log('Added series for ', attrs.name );
             }
         }
     })
     .directive('logGauge', function() {
         return {
-            restrict: 'E',
-            template: '<div class="log-gauge"></div>',
-            transclude:true,
-            replace: true,
-
             link: function (scope, element, attrs) {
 
                 var chart = new Highcharts.Chart({
@@ -223,10 +222,9 @@ angular.module('brewberry.directive', [])
                     },
                 
                     series: [{
-                        name: 'Temperature',
                         data: [20],
                         tooltip: {
-                            valueSuffix: '°C'
+                            enabled: false
                         },
                         dataLabels: {
                             formatter: function () {
@@ -236,11 +234,10 @@ angular.module('brewberry.directive', [])
                     }]
                 
                 });
-                scope.$watch("sample", function(sample) {
+                scope.$watch(attrs.sample, function(sample) {
                     if (sample) {
-                        console.log('gauge', sample);
                         var point = chart.series[0].points[0];
-                        point.update(sample.temperature);
+                        point.update(sample[attrs.value]);
                     }
                 });
             }
