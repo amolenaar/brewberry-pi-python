e233a0869b6473732d5d3846b4affdd9c5159540
Arjan Molenaar <gaphor@gmail.com>
2013-07-05 09:58:15 +0200

    Added boilerplate for state machine.

--- brewberry/controller.py
+++ brewberry/controller.py
@@ -8,10 +8,11 @@ JOULES_1_LITRE = 4186
 
 class Config(object):
 
-    def __init__(self, power = 2000, efficiency = .5, litres = 17):
-        self.power = power
+    def __init__(self, power = 2000, efficiency = .5, volume = 17):
+        self.power = power # Watts
         self.efficiency = efficiency
-        self.litres = litres
+        self.volume = volume # litres
+        self.slack = 120 # seconds
 
     def time(self, dtemp):
         return (JOULES_1_LITRE * self.litres) / (self.power * self.efficiency)
@@ -39,5 +40,44 @@ class Controller(object):
         else:
             if io.read_heater():
                 io.set_heater(False)
+
+
+# State machine:
+class State(object):
+
+    def __init__(self, io):
+        self.io = io
+
+
+class Heating(State):
+    """
+    . calculate time to heat
+    . Heat as long as the timer has not elapsed
+    """
+
+    def next(self):
+        return Slacking()
+
+
+class Slacking(State):
+    """
+    . Keep track of x last temperatures
+    . Define dtemp over x temperature readings
+    . If dtemp < y go to next stage
+    """
+
+    def next(self):
+        return Resting()
+
+
+class Resting(State):
+    """
+    . keep track of last temperature reading
+    . If t_mash < y-d degrees, go to next stage
+    """
+
+    def next(self):
+        return Heating()
+
 
 # vim:sw=4:et:ai
